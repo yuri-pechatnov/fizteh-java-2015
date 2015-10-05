f39bbd41e22e5998f8f9af095e815dad9a773a41
ypechatnov <yuri.pechatnov@gmail.com>
2015-10-05 16:15:41 +0300

    Twitter stream. Deleting strange folder

--- ypechatnov/src/main/java/ru/fizteh/fivt/students/ypechatnov/ypechatnov/TwitterParameterException.java
+++
@@ -1,7 +0,0 @@
-package main.java.ru.fizteh.fivt.students.ypechatnov.ypechatnov.exceptions;
-
-/**
- * Created by ura on 04.10.15.
- */
-public class TwitterParameterException extends Exception {
-}
