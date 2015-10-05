cfe0a1573f22e05bf6d8c8cc4e4eb04cc3afaaad
ypechatnov <yuri.pechatnov@gmail.com>
2015-10-05 11:02:31 +0300

    Twitter Streream. Something slightly better

---
+++ ypechatnov/src/main/java/ru/fizteh/fivt/students/ypechatnov/exceptions/TwitterParameterException.java
@@ -0,0 +1,7 @@
+package main.java.ru.fizteh.fivt.students.ypechatnov.exceptions;
+
+/**
+ * Created by ura on 04.10.15.
+ */
+public class TwitterParameterException {
+}
---
+++ ypechatnov/src/main/java/ru/fizteh/fivt/students/ypechatnov/validators/NonNegativeInteger.java
@@ -0,0 +1,7 @@
+package main.java.ru.fizteh.fivt.students.ypechatnov.validators;
+
+/**
+ * Created by ura on 04.10.15.
+ */
+public class NonNegativeInteger {
+}
